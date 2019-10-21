bff0e66c6d2c6965fc06abee77ae076e56aa2a8e
Sergey Surin <sergey.surin@boldandreeves.co.uk>
2019-10-21 11:56:25 +0300

    Binds the Tochka routes

--- src/Tochka.php
+++ src/Tochka.php
@@ -4,6 +4,9 @@
 namespace MrSayrax\Tochka;
 
 
+use Illuminate\Support\Facades\Route;
+use MrSayrax\Tochka\RouteRegistrar;
+
 class Tochka extends TochkaClient
 {
 
@@ -43,6 +46,31 @@ class Tochka extends TochkaClient
     public function getBankStatement(array $params)
     {
         return $this->postResponse('/v1/statement', $params);
+    }
+
+    /**
+     * Binds the Tochka routes into the controller.
+     *
+     * @param  callable|null  $callback
+     * @param  array  $options
+     * @return void
+     */
+    public static function routes($callback = null, array $options = [])
+    {
+        $callback = $callback ?: function ($router) {
+            $router->all();
+        };
+
+        $defaultOptions = [
+            'prefix' => 'tochka',
+            'namespace' => '\MrSayrax\Tochka\Http\Controllers',
+        ];
+
+        $options = array_merge($defaultOptions, $options);
+
+        Route::group($options, function ($router) use ($callback) {
+            $callback(new RouteRegistrar($router));
+        });
     }
 
 }
